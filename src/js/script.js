0c75d154e35a17a8281c17a11ada939bf25474e9
kazzmatazz <crows.worst.1982@gmail.com>
2023-10-26 06:43:25 +0900

    メインビューまで

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  // ハンバーガーメニュー
+  $(".js-hamburger,.js-drawer,.js-drawer a").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
 
-
+  // スワイパー
+  const swiper = new Swiper(".swiper1", {
+    effect: "fade",
+    speed: 3000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3000,
+    },
+  });
 });

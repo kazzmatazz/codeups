be4fd3b490f7050de39a986efccbd49e390adaf9
kazzmatazz <crows.worst.1982@gmail.com>
2023-11-01 08:05:40 +0900

    パーフェクトピクセルまで

--- src/js/script.js
+++ src/js/script.js
@@ -33,20 +33,15 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   });
 
   // アニメ出現
-  //要素の取得とスピードの設定
   let box = $('.colorbox'),
   speed = 700;
-
-  //.colorboxの付いた全ての要素に対して下記の処理を行う
   box.each(function(){
   $(this).append('<div class="color"></div>')
-  var color = $(this).find($('.color')),
+  let color = $(this).find($('.color')),
   image = $(this).find('img');
-  var counter = 0;
-
+  let counter = 0;
   image.css('opacity','0');
   color.css('width','0%');
-  //inviewを使って背景色が画面に現れたら処理をする
   color.on('inview', function(){
     if(counter == 0){
     $(this).delay(200).animate({'width':'100%'},speed,function(){
